88dc779e77bb8241dda30076df57ce1f0c8379d2
sarahraquely <160151647+sarahraquely@users.noreply.github.com>
2024-06-14 12:35:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+var a = 10;
+var b = 15;
+
+var c = a + b;
+
+console.log(c);
+*/
+
+/*
+var a = 10;
+var b = 15;
+
+var c = a - b;
+
+console.log(c);
+*/
+
+/*
+var a = 10;
+var b = 15;
+
+var c = a * b;
+
+console.log(c);
+*/
